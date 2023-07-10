d2fdaf1f892123c41ef7b45b75f7753e95c6d2b8
unknown <mahadymahamudh472@gmail.com>
2023-07-10 08:09:15 -0700

    Signed-off-by: unknown <mahadymahamudh472@gmail.com>

---
+++ hexweb/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+def about(request):
+    return render(request, 'about.html')
+def partnership(request):
+    return render(request, 'partnership.html')
+def risk_map(request):
+    return render(request, 'risk-map.html')
+def travel_hub(request):
+    return render(request, 'travel_hub.html')
+
